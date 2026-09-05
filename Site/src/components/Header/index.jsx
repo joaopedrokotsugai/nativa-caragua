6408afa2d03d16190174d5ef9c2eaa2322345e4e
joaokotsugai <joao.kotsugai@aluno.ifsp.edu.br>
2026-09-05 11:42:16 -0300

    criando estrutura inicial do frontend

---
+++ Site/src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+export default function Header() {
+  return (
+    <header>
+      <div className="Header">
+        <h1>Bem-vindo ao Nativa Caraguá</h1>
+      </div>
+
+
+    </header>
+  );
+}
---
+++ Site/src/routes/AppRoutes.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router";
+
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
